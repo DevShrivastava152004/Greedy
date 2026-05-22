3c0c70419298cce7c69d50e5d8ea67148514a9f5
Dev Shrivastava <devshrivastava621@gmail.com>
2026-05-22 12:49:29 +0530

    Partition Labels Problem Solved

---
+++ PartitionLabels.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class PartitionLabels {
+    public List<Integer> partitionLabels(String s)
+    {
+        int n = s.length();
+        List<Integer> result = new ArrayList<>();
+        int[] lastIndex = new int[26];
+
+        for(int i=0;i<n;i++)
+        {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+        int i=0;
+        int start = 0;
+        int end = 0;
+        while(i < n)
+        {
+            end = Math.max(end, lastIndex[s.charAt(i) - 'a']);
+            if(i==end)
+            {
+                result.add(end-start+1);
+                start = i+1;
+            }
+            i++;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        PartitionLabels solution = new PartitionLabels();
+        String s = "ababcbacadefegdehijhklij";
+        List<Integer> result =  solution.partitionLabels(s);
+        System.out.println(result);
+    }
+}
